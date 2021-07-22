3552163f54d211aa2081905b0d950c214209e357
Christopher Cohen <chris@cscohen.com>
2021-07-22 16:12:31 -0600

    commit tree 24a74a167ae571b6beb3153473dd506f7edd9fdf

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(el => el.toLowerCase() === name.toLowerCase())
+    }
+    
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(el => el.startsWith(letters))
+    }
+    
+function matchName(drivers,  match) {
+    return drivers.filter(el => el.name === match)
+    }
